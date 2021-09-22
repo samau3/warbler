247b6d803ee1d375af0a633db8865d1244a97fd9
samau3 <samau415@gmail.com>
2021-09-22 10:55:18 -0700

    initial commit, added logout route logic and CSRF form

---
+++ forms.py
@@ -0,0 +1,32 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, TextAreaField
+from wtforms.validators import DataRequired, Email, Length
+
+
+class MessageForm(FlaskForm):
+    """Form for adding/editing messages."""
+
+    text = TextAreaField('text', validators=[DataRequired()])
+
+
+class UserAddForm(FlaskForm):
+    """Form for adding users."""
+
+    username = StringField('Username', validators=[DataRequired()])
+    email = StringField('E-mail', validators=[DataRequired(), Email()])
+    password = PasswordField('Password', validators=[Length(min=6)])
+    image_url = StringField('(Optional) Image URL')
+
+
+class LoginForm(FlaskForm):
+    """Login form."""
+
+    username = StringField('Username', validators=[DataRequired()])
+    password = PasswordField('Password', validators=[Length(min=6)])
+
+class OnlyCsrfForm(FlaskForm):
+    """For actions where we want CSRF protection, but don't need any fields.
+
+    Currently used for our "delete" buttons, which make POST requests, and the
+    logout button, which makes POST requests.
+    """
